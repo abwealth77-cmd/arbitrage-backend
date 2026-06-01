4cfc7b8cd680ddc03e41e613129a16081dc4cedd
abwealth77-cmd <abwealth77@gmail.com>
2026-06-01 14:50:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -209,6 +209,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
+  startScheduler();
 });
 function startScheduler() {
   let interval = 60 * 1000;
